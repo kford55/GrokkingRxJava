b629fdd7bab9ca86363295926b0f601d96b20f4f
Ford <Kenneth.Ford@capitalone.com>
2016-12-19 13:38:20 -0500

    Using operators

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,11 @@
 import rx.Observable;
+import rx.functions.Func1;
 
 public class Main {
 
   public static void main (String[] args) {
     Observable.just("Hello, world!")
+            .map(s -> s + " -Ken")
             .subscribe(s -> System.out.println(s));
   }
 }
