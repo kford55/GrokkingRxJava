86d941b3f7f9547da423e83f680fd051be9ae2ff
Ford <Kenneth.Ford@capitalone.com>
2016-12-19 13:49:32 -0500

    Finished chapter 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,8 @@ public class Main {
   public static void main (String[] args) {
     Observable.just("Hello, world!")
             .map(s -> s + " -Ken")
+            .map(s -> s.hashCode())
+            .map(i -> Integer.toString(i))
             .subscribe(s -> System.out.println(s));
   }
 }
